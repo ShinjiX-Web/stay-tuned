56c302036d1f3cb38ab2d31b023eec8d64ba1e45
ShinjiX-Web <npanares4@gmail.com>
2023-08-18 10:10:19 +0800

    Version 1.2

--- subscribe.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  // Process the form data (you can save it to a database, send emails, etc.)
-
-  // Redirect to the thank you page
-  header("Location: thank-you.html");
-  exit;
-}
-?>
